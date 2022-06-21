8477e5abbb84b454e31510b7afabc14e91ee4341
JeffAllison <itsjallison@gmail.com>
2022-06-21 15:49:43 -0700

    added authentication and authorization to .get path in users controllers file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,11 +56,11 @@ describe('lazy-bouncer routes', () => {
     });
   });
 
-  it.skip('should return a 401 when signed out and listing all users', async () => {
+  it('should return a 401 when signed out and listing all users', async () => {
     const res = await request(app).get('/api/v1/users');
 
     expect(res.body).toEqual({
-      message: 'You must be signed in to continue',
+      message: 'Sign In To Continue',
       status: 401,
     });
   });
